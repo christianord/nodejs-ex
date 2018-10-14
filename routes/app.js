acef974c6655bb8e801d7fcbaef1adaa23789c82
CHROMACHINE\christianord <christianord@gmail.com>
2018-10-13 22:00:40 -0300

    clave db

--- routes/app.js
+++ routes/app.js
@@ -4,7 +4,16 @@ var app = express();
 
 app.get('/', function(req, res) {
 
+    var connection_string = "No encontro nada";
+    // if OPENSHIFT env variables are present, use the available connection info:
+    if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
 
+        connection_string = process.env.MONGODB_USERNAME + ":" +
+            process.env.MONGODB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.MONGODB_DATABASE;
+    }
     res.status(200).json({
         ok: true,
         mensaje: 'Peticion realizada correctamente'

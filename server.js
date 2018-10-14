b704c08fbfe6533d4c77e90d03a93b547fb65ee6
CHROMACHINE\christianord <christianord@gmail.com>
2018-10-13 21:45:43 -0300

    db

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
     connection_string =
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
+        process.env.MONGODB_DATABASE;
 }
 
 mongoose.connection.openUri('mongodb://' + connection_string, (err, res) => {
